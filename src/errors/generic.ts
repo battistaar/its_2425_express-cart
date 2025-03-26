c58aaeed15eddec8439217baa11b732cc3116b32
Enrico Battistella <enrico.battistella@digitalrockers.it>
2025-03-26 14:48:13 +0100

    fix

--- src/errors/generic.ts
+++ src/errors/generic.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 export const genericHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err);
     res.status(500).json({
-        name: err.name,
+        error: err.name,
         message: err.message
     });
 }
