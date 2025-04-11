c8250341fc98d5e62690d5f485a50ee070eaeed6
Enrico Battistella <enrico.battistella@digitalrockers.it>
2025-04-11 12:03:16 +0200

    api me

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import productRouter from './product/product.router';
 import cartRouter from './cart-item/cart-item.router';
+import userRouter from './user/user.router';
 import authRouter from './auth/auth.router';
 
 const router = Router();
 
 router.use('/products', productRouter);
 router.use('/cart', cartRouter);
+router.use('/users', userRouter);
 router.use(authRouter);
 
 export default router;
